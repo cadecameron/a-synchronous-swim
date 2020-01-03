8a392318da0a7a9d6c2e14172212bfac20779415
cadecameron <cade.cameron@gmail.com>
2020-01-03 15:15:45 -0800

    Cades solo work -- mainly comments from Fred lecture

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,49 +1,58 @@
-(function() {
+(function () {
 
   const serverUrl = 'http://127.0.0.1:3000';
 
   //
   // TODO: build the swim command fetcher here
+  // GET request function that sends the request
+  // the function will be on a setTimer, so that the client continually
+  // asks the server for any commands it needs to action.
   //
 
+  const ajaxFetchCommand = () =
+  .ajax...
+    success: (command) => {
+  SwimTime.move(command)
+}
+
   /////////////////////////////////////////////////////////////////////
-  // The ajax file uplaoder is provided for your convenience!
+  // The ajax file uploader is provided for your convenience!
   // Note: remember to fix the URL below.
   /////////////////////////////////////////////////////////////////////
 
-  const ajaxFileUplaod = (file) => {
-    var formData = new FormData();
-    formData.append('file', file);
-    $.ajax({
-      type: 'POST',
-      data: formData,
-      url: 'FILL_ME_IN',
-      cache: false,
-      contentType: false,
-      processData: false,
-      success: () => {
-        // reload the page
-        window.location = window.location.href;
-      }
-    });
-  };
-
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-
-    var form = $('form .file')[0];
-    if (form.files.length === 0) {
-      console.log('No file selected!');
-      return;
+  const ajaxFileUpload = (file) => {
+  var formData = new FormData();
+  formData.append('file', file);
+  $.ajax({
+    type: 'POST',
+    data: formData,
+    url: serverUrl,
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: () => {
+      // reload the page
+      window.location = window.location.href;
     }
-
-    var file = form.files[0];
-    if (file.type !== 'image/jpeg') {
-      console.log('Not a jpg file!');
-      return;
-    }
-
-    ajaxFileUplaod(file);
   });
+};
 
-})();
+$('form').on('submit', function (e) {
+  e.preventDefault();
+
+  var form = $('form .file')[0];
+  if (form.files.length === 0) {
+    console.log('No file selected!');
+    return;
+  }
+
+  var file = form.files[0];
+  if (file.type !== 'image/jpeg') {
+    console.log('Not a jpg file!');
+    return;
+  }
+
+  ajaxFileUplaod(file);
+});
+
+}) ();
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -12,9 +12,20 @@ module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
 
+// this function currently only handles an OPTIONS request
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  res.writeHead(200, headers);
+  res.writeHead(200, headers); // var randomCmd = ??; res.end(randomCmd)
+  res.write('Welcome to my server!') // res.write puts things to the DOM
   res.end();
   next(); // invoke next() at the end of a request to help with testing!
 };
+
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO ////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+// add functions that handle different get methods (i.e. one function for 'up',
+// another function for 'down', etc.)
